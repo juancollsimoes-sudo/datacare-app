7c3429611e2933ec402e2c87a56b78b161399458
juanchito <juancollsimoes@gmail.com>
2026-08-15 22:58:12 -0400

    chore: clean up project files

--- patch2.py
+++
@@ -1,9 +0,0 @@
-with open('rust/src/db/repository.rs', 'r') as f:
-    lines = f.readlines()
-
-for i, line in enumerate(lines):
-    if 'Ok(PaginatedSesiones {' in line and i < 100:  # line 74 is Pacientes
-        lines[i] = line.replace('PaginatedSesiones', 'PaginatedPacientes')
-
-with open('rust/src/db/repository.rs', 'w') as f:
-    f.writelines(lines)
