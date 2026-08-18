9a0498371106c1019cd085a2551c30259ba1f958
juanchito <juancollsimoes@gmail.com>
2026-08-18 18:45:41 -0400

    chore: bump version to v1.0.2

--- lib/src/core/constants/app_constants.dart
+++ lib/src/core/constants/app_constants.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 
 class AppConstants {
   static const String appName = 'DataCare';
-  static const String appVersion = 'v1.0.1';
+  static const String appVersion = 'v1.0.2';
   
   // Color de marca (rosa mauve — Beauty Sweet Care Spa)
   static const Color brandColor = Color(0xFFC77D9C);
